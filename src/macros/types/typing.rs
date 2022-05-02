db27ab1a5e9898740d89d0385c093a478828beaf
BButner <butner.beau@gmail.com>
2022-05-02 13:15:40 -0500

    This file didn't get checked in

---
+++ src/macros/types/typing.rs
@@ -0,0 +1,31 @@
+use std::env;
+use enigo::{Enigo, Key, KeyboardControllable};
+
+enum TypingMode {
+    // Windows
+    KeyDownUp,
+    // macOS
+    KeySequence,
+}
+
+pub fn type_macro(args: &Vec<String>) {
+
+    let mode: TypingMode =
+        if env::consts::OS == "macos" { TypingMode::KeySequence } else { TypingMode::KeyDownUp };
+
+    let string_to_type = &args.join(" ");
+
+    let mut enigo = Enigo::new();
+
+    match mode {
+        TypingMode::KeySequence => {
+            enigo.key_sequence(string_to_type);
+        }
+        TypingMode::KeyDownUp => {
+            for c in string_to_type.chars() {
+                enigo.key_down(Key::Layout(c));
+                enigo.key_up(Key::Layout(c));
+            }
+        }
+    }
+}
