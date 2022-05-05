9d5e46aae04f3b9e1e2eafea6a8c4f0d8aedbc57
BButner <butner.beau@gmail.com>
2022-05-05 14:51:20 -0500

    testing

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,16 +1,21 @@
 use std::time::Duration;
 use eframe::{egui, Storage};
 use eframe::emath::Vec2;
-use eframe::epaint::Rgba;
+use eframe::epaint::{Color32, Rgba};
 use eframe::glow::Context;
-use egui::{FontFamily, TextEdit, Visuals, RichText, FontId, Button};
+use egui::{FontFamily, TextEdit, Visuals, RichText, FontId, Button, Stroke};
 use crate::config::Config;
+use crate::macros::type_macro;
+
+const WINDOW_WIDTH: f32 = 500.0;
+const BUTTON_HEIGHT: f32 = 20.0;
+const TEXT_HEIGHT: f32 = 36.0;
 
 pub fn launch_gui(config: Config) {
     let options = eframe::NativeOptions {
         transparent: true,
         decorated: false,
-        initial_window_size: Option::from(Vec2 { x: 500.0, y: 200.0 }),
+        initial_window_size: Option::from(Vec2 { x: WINDOW_WIDTH, y: 80.0 }),
         ..eframe::NativeOptions::default()
     };
 
@@ -35,11 +40,12 @@ impl eframe::App for MacroBar {
     // }
 
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("")
+        egui::CentralPanel::default()
             .frame(egui::Frame::none())
             .show(ctx, |ui| {
                 let text_len = self.current_text.len();
                 let text = &self.current_text.clone();
+                let mut additional_height = 0.0;
 
                 let textedit =
                     TextEdit::singleline(&mut self.current_text)
@@ -47,23 +53,28 @@ impl eframe::App for MacroBar {
                         .hint_text("Macro Key")
                         .font(FontId { size: 32.0, family: Default::default() });
 
-                ui.shrink_height_to_current();
-                ui.shrink_width_to_current();
                 ui.vertical(|ui| {
-                    ui.label("Testing");
                     ui.add(textedit);
 
                     if text_len > 1 {
-                        println!("testing");
-                        println!("{:?}", self.config.typing_macros);
-
                         &for x in self.config.typing_macros
                             .iter()
-                            .filter(|m| m.key.contains(text)) {
-                            ui.add(Button::new(&x.key));
+                            .filter(|m| m.key.contains(text))
+                            .take(5) {
+                            let button = Button::new(&x.key)
+                                .stroke(Stroke { width: 0.0, color: Default::default() });
+
+                            additional_height += BUTTON_HEIGHT;
+
+                            if ui.add(button).clicked() {
+                                frame.quit();
+                                type_macro(&x.text);
+                            }
                         };
                     }
                 });
+
+                // frame.set_window_size(Vec2::new(WINDOW_WIDTH, TEXT_HEIGHT + additional_height));
             });
     }
 }
